1903d5074a886fae54cd25d0c1406e99e5966acd
Kritika <kritikav857@gmail.com>
2026-08-14 23:44:26 +0530

    implement registration validation and local storage

---
+++ client/js/register.js
@@ -0,0 +1,56 @@
+const form=document.getElementById("registerForm");
+
+const namevalue=document.getElementById("name");
+const emailvalue=document.getElementById("email");
+const passvalue=document.getElementById("password");
+const confirmpassvalue=document.getElementById("confirmPassword");
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const errorbox=document.getElementById("errors");
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    errorbox.textContent = "";
+    const errors=[];
+    const name=namevalue.value;
+    if(name.trim()===""){
+        errors.push("Name is required");
+    }
+    const email=emailvalue.value;
+    if(email.trim()===""){
+        errors.push("Email is required");
+    }
+    else if(!emailPattern.test(email)){
+        errors.push("Invalid Email");
+    }
+   const pass=passvalue.value;
+    if(pass.length<8){
+        errors.push("Password must be atleast 8 characters");
+    }
+   const confirmpass=confirmpassvalue.value;
+    if(pass!==confirmpass){
+        errors.push("Passwords do not match");
+    }
+   if(errors.length>0){
+    errorbox.textContent=errors.join("\n");
+    return;
+   }
+   
+    const user = {
+    id: Date.now(),
+    name: name,
+    email: email,
+    password: pass,
+    avatar: "",
+    skills: [],
+    bio: "",
+    createdAt: new Date().toISOString()
+};
+    const storedUsers = localStorage.getItem("users");
+    let users = storedUsers ? JSON.parse(storedUsers) : [];
+
+    users.push(user);
+
+    localStorage.setItem("users", JSON.stringify(users));
+   
+
+
+})
